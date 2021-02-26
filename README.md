aff92c75ebfcd5ce778d4a075b8416dc6ba6bf9a
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-02-26 13:40:50 +0000

    return a number

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 run test `go test`
 <br>
 run file `go run filename.go`
+
+3 is fizz
+5 is buzz
+15 is fizzbuzz
+if not any above return number
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+func TestFizzBuzz(t *testing.T) {
+	got := fizzBuzz(1)
+	want := 1
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
