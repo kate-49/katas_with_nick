8ad9082e393e560dd59b43c91532a55b4c15825e
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-26 13:50:30 +0000

    failing test for Fizz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,3 +1,5 @@
 package main
 
-import "fmt"
+func fizzBuzz(input int) int {
+	return input
+}
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -2,9 +2,18 @@ package main
 
 import "testing"
 
-func TestFizzBuzz(t *testing.T) {
+func returnIntIfNotFizzbuzz(t *testing.T) {
 	got := fizzBuzz(1)
 	want := 1
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func returnFizzFor3(t *testing.T) {
+	got := fizzBuzz(3)
+	want := "Fizz"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
