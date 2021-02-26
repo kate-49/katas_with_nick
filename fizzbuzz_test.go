d7db444a341a22e91ae2ca1758ea71567d942fb7
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-02-26 14:35:34 +0000

    failing to get fizzbuzz

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,27 +1,38 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func checkEquality(t *testing.T, got, want string) {
-	if got != want {
+ 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
 
-func TestIntIfNotFizzbuzz(t *testing.T) {
-	got := fizzBuzz(1)
-	want := "1"
-	checkEquality(t, got, want)
-}
+func TestFizzBuzz(t *testing.T) {
+	t.Run("TestIntIfNotFizzbuzz", func(t *testing.T) {
+		got := fizzBuzz(1)
+		want := "1"
+		checkEquality(t, got, want)
 
-func TestFizzFor3(t *testing.T) {
-	got := fizzBuzz(3)
-	want := "Fizz"
-	checkEquality(t, got, want)
-}
+	})
 
-func TestFizzFor5(t *testing.T) {
-	got := fizzBuzz(5)
-	want := "Buzz"
-	checkEquality(t, got, want)
+	t.Run("TestFizzFor3", func(t *testing.T) {
+		got := fizzBuzz(3)
+		want := "Fizz"
+		checkEquality(t, got, want)
+	})
+
+	t.Run("TestFizzFor5", func(t *testing.T) {
+		got := fizzBuzz(5)
+		want := "Buzz"
+		checkEquality(t, got, want)
+	})
+
+	t.Run("TestFizzFor15", func(t *testing.T) {
+		got := fizzBuzz(15)
+		want := "FizzBuzz"
+		checkEquality(t, got, want)
+	})
 }
