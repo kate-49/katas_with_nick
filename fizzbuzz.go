09fdde99da8328ecd6f5e17d454fa9ff418e0091
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-02-26 14:55:49 +0000

    refactor tests for a provider

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -5,11 +5,11 @@ import (
 )
 
 func fizzBuzz(input int) string {
-	if input == 15 {
+	if input%15 == 0 {
 		return "FizzBuzz"
-	} else if input == 3 {
+	} else if input%3 == 0 {
 		return "Fizz"
-	} else if input == 5 {
+	} else if input%5 == 0 {
 		return "Buzz"
 	} else {
 		return strconv.Itoa(input)
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -4,40 +4,21 @@ import (
 	"testing"
 )
 
-func checkEquality(t *testing.T, got, want string) {
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
 func TestFizzBuzz(t *testing.T) {
-	t.Run("TestIntIfNotFizzbuzz", func(t *testing.T) {
-		got := fizzBuzz(1)
-		want := "1"
-		checkEquality(t, got, want)
-	})
+	FizzBuzzProvider := []struct {
+			Got int
+			Want string
+	}{
+		{Got: 1, Want: "1"},
+		{Got: 3, Want: "Fizz"},
+		{Got: 5, Want: "Buzz"},
+		{Got: 15, Want: "FizzBuzz"},
+	}
 
-	t.Run("TestFizzFor3", func(t *testing.T) {
-		got := fizzBuzz(3)
-		want := "Fizz"
-		checkEquality(t, got, want)
-	})
-
-	t.Run("TestFizzFor5", func(t *testing.T) {
-		got := fizzBuzz(5)
-		want := "Buzz"
-		checkEquality(t, got, want)
-	})
-
-	t.Run("TestFizzFor15", func(t *testing.T) {
-		got := fizzBuzz(15)
-		want := "FizzBuzz"
-		checkEquality(t, got, want)
-	})
-
-	t.Run("TestFizzFor30", func(t *testing.T) {
-		got := fizzBuzz(30)
-		want := "FizzBuzz"
-		checkEquality(t, got, want)
-	})
+	for _, FizzBuzzInput := range FizzBuzzProvider {
+		got := fizzBuzz(FizzBuzzInput.Got)
+		if FizzBuzzInput.Want != got {
+			t.Errorf("got %q want %q", FizzBuzzInput.Got, FizzBuzzInput.Want)
+		}
+	}
 }
